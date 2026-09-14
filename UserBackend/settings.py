33504bae611fe7d91b66307a9d6fd4df9dc398a4
Parvati <parvathynair186@gmail.com>
2026-09-14 19:45:52 +0530

    'settings-updated'

--- UserBackend/settings.py
+++ UserBackend/settings.py
@@ -118,6 +118,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5174',
     'http://127.0.0.1:5175',
     'http://127.0.0.1:5173',
+    'https://e-learningadmin-fe.netlify.app',
+]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.netlify\.app$",
 ]
 CORS_ALLOW_ALL_ORIGINS = DEBUG
 CORS_ALLOW_CREDENTIALS = True
